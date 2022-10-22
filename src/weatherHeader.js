730cc51a1217941ded93d12760cc28c743ad00d8
erjaylee9 <sailaway009@gmail.com>
2022-10-22 09:07:38 -0400

    unit conversion

--- src/weatherHeader.js
+++ src/weatherHeader.js
@@ -1,25 +1,48 @@
-import React from "react";
+import React,{useState} from "react";
 export default function Header(props) {
-  return (
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  if (unit === `celsius`) {
+
+    return (
       <div className="Header">
         <div className="heading">
           <h1>
-            <img
-              src={props.iconUrl}
-              alt={props.description}
-            />
-          <br /> <div id="city">{props.city} </div>
-          <div id="current-time"></div>
+            <img src={props.iconUrl} alt={props.description} />
+            <br /> <div id="city">{props.city} </div>
+            <div id="current-time"></div>
           </h1>
           <h2>
             <span className="temperature"> {props.temperature} </span>
-             
-            <span className="unit">
-           
-               °C 
-            </span>
+
+            <span className="unit">°C |<a href="/" onClick={showFahrenheit}>°F </a></span>
           </h2>
         </div>
       </div>
-  );
+    );
+  } else {
+    let fahrenheit = (props.temperature * 9) / 5 + 32;
+    return (
+      <div className="Header">
+        <div className="heading">
+          <h1>
+            <img src={props.iconUrl} alt={props.description} />
+            <br /> <div id="city">{props.city} </div>
+            <div id="current-time"></div>
+          </h1>
+          <h2>
+            <span className="temperature"> {Math.round(fahrenheit)} </span>
+            <span className="unit"> <a href="/" onClick={showCelsius}>°C</a>{" "} | °F</span>
+          </h2>
+        </div>
+      </div>
+    );
+  }
 }
